44214165e9592ab2fe8c984229849d44faaecf56
wilmergit <wilmerzeasrodriguez@gmail.com>
2023-12-05 20:22:45 +0000

    Universe final

--- js/formulary-validation.js
+++ js/formulary-validation.js
@@ -1,38 +1,40 @@
 
 
-function validateForm() {
+function validateForm(event) {
 
   let username = document.getElementById('input-username').value;
   let email = document.getElementById('input-email').value;
   let password = document.getElementById('input-password').value;
 
   if (username == "") {
-    alert('Username required');
-    return false;
+    event.preventDefault();
+    document.getElementById("error-username-required").style.display = "block";
+  }
+  else{
+    document.getElementById("error-username-required").style.display = "none";
   }
 
   if (email == "") {
-    alert('Email required');
-    return false;
+    event.preventDefault();
+    document.getElementById("error-email-required").style.display = "block";
   }
-  else if (!email.includes("@")) {
-    alert('Invalid email address');
-    return false;
+  else{
+    document.getElementById("error-email-required").style.display = "none";
   }
 
   if (password == "") {
-    alert('Password required');
-    return false;
+    event.preventDefault();
+    document.getElementById("error-password-required").style.display = "block";
+  }else{
+    document.getElementById("error-password-required").style.display = "none";
   }
 
   return true;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  readData();
-  }); 
 
-function readData(){
+
+function readData() {
   let userList;
   if (localStorage.getItem('userList') == null) {
     userList = [];
@@ -41,20 +43,20 @@ function readData(){
     userList = JSON.parse(localStorage.getItem('userList'));
   }
 
-  var html = "";
+  let html = "";
 
   userList.forEach(function (element, index) {
     html += "<tr>";
-    html += "<td>"+ element.email + "</td>";
-    html += "<td>"+ element.username + "</td>";
-    html += '<td><button onclick="deleteData('+ index +')" class="btn btn-danger">Eliminar</button> <button onclick="editData('+ index +')" class="btn btn-warning">Editar</button><td>';
+    html += "<td>" + element.email + "</td>";
+    html += "<td>" + element.username + "</td>";
+    html += '<td><button onclick="deleteData(' + index + ')" class="btn btn-danger">Eliminar</button> <button onclick="editData(' + index + ')" class="btn btn-warning">Editar</button><td>';
     html += "</tr>";
   });
 
   document.getElementById('table-data').innerHTML = html;
 }
 
-document.onload = readData(); 
+document.onload = readData();
 
 function addData() {
   if (validateForm() == true) {
@@ -62,7 +64,7 @@ function addData() {
     let email = document.getElementById('input-email').value;
 
     let userList;
-    
+
     if (localStorage.getItem('userList') == null) {
       userList = [];
     }
@@ -76,21 +78,21 @@ function addData() {
     });
 
     localStorage.setItem('userList', JSON.stringify(userList));
-    
+
     readData();
-    
+
     document.getElementById('input-email').value = "";
     document.getElementById('input-username').value = "";
 
   }
 }
 
-  function deleteData(index){
+function deleteData(index) {
   let userList;
 
-  if (localStorage.getItem('userList') == null){
+  if (localStorage.getItem('userList') == null) {
     userList = [];
-  }else{
+  } else {
     userList = JSON.parse(localStorage.getItem('userList'));
   }
 
@@ -100,38 +102,36 @@ function addData() {
   readData();
 }
 
-function editData(index){
+function editData(index) {
   document.getElementById('btn-add').style.display = 'none';
   document.getElementById('btn-update').style.display = 'block';
 
   let userList;
-    
-    if (localStorage.getItem('userList') == null) {
-      userList = [];
+
+  if (localStorage.getItem('userList') == null) {
+    userList = [];
+  }
+  else {
+    userList = JSON.parse(localStorage.getItem('userList'));
+  }
+
+  document.getElementById('input-email').value = userList[index].email;
+  document.getElementById('input-username').value = userList[index].username;
+
+  document.querySelector('#btn-update').onclick = function () {
+    if (validateForm() == true) {
+      userList[index].email = document.getElementById('input-email').value;
+      userList[index].username = document.getElementById('input-username').value;
+
+      localStorage.setItem('userList', JSON.stringify(userList));
+      readData();
+
+      document.getElementById('input-email').value = "";
+      document.getElementById('input-username').value = "";
+
+      document.getElementById('btn-add').style.display = 'block';
+      document.getElementById('btn-update').style.display = 'none';
     }
-    else {
-      userList = JSON.parse(localStorage.getItem('userList'));
-    }
-
-    document.getElementById('input-email').value = userList[index].email;
-    document.getElementById('input-username').value = userList[index].username;
-
-    document.querySelector('#btn-update').onclick = function (){
-      if (validateForm() = true){
-        userList[index].email = document.getElementById('input-email').value;
-        userList[index].username = document.getElementById('input-username').value;
-
-        localStorage.setItem('userList', JSON.stringify(userList));
-        readData();
-
-        document.getElementById('input-email').value = "";
-        document.getElementById('input-username').value = "";
-
-        document.getElementById('btn-add').style.display = 'block';
-        document.getElementById('btn-update').style.display = 'none';
-      }
-    }
-
-
+  }
 }
 
